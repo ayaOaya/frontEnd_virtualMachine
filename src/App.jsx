da9ae43f2948ada51ad9567274eec563c69bc335
ayaOaya <alyaauaacn@gmail.com>
2023-04-19 01:21:10 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import SignUp from "../src/UsersInfo/signUp/SignUp"
+import LogIn from "../src/UsersInfo/logIn/LogIn"
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      
+     
+    </div>
+  )
+}
+
+export default App
